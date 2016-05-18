136a65dbed16af32ba5108844da8cbc9510212cd
Luis Alberto Pena Nunez <luis@megsoftconsulting.com>
2016-05-17 22:15:25 -0500

    Implemented the Login page in code-behind

---
+++ XForms/Screens/Login/Code-behind/LoginScreen.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace XForms
+{
+	public class LoginScreen : ContentPage
+	{
+		public LoginScreen ()
+		{
+			Content = CreatePageContent();
+		}
+
+		public View CreatePageContent ()
+		{
+			var logo = new Image
+			{
+				Source = "megsoft",
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				HeightRequest = 200,
+				Aspect = Aspect.AspectFit
+			};
+
+			var title = new Label
+			{
+				Text = "Megsoft Consulting",
+				TextColor = Color.FromHex("13a3e2"),
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				FontAttributes = FontAttributes.Bold,
+				FontSize = 32
+			};
+
+			var subtitle = new Label
+			{
+				Text = "We make your dream come true",
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				TextColor = Color.Gray
+			};
+
+			var firstLayout = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				Children = 
+				{
+					logo,
+					title,
+					subtitle
+				}
+			};
+
+			var facebookButton = new StackLayout
+			{
+				Children = 
+				{
+					new StackLayout
+					{
+						Orientation = StackOrientation.Horizontal,
+						Children = 
+						{
+							new Button
+							{
+								HorizontalOptions = LayoutOptions.CenterAndExpand,
+								BackgroundColor = Color.FromHex("3b5998"),
+								BorderRadius = 4,
+								Text = "Login using Facebook",
+								WidthRequest = 200,
+								TextColor = Color.White
+							}
+						}
+					}
+				}
+			};
+
+			var releaseVersion = new Label
+			{
+				Text = "Demo 1.0",
+				TextColor = Color.Gray,
+				FontSize = 10,
+				HorizontalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			var secondLayout = new StackLayout
+			{
+				Spacing = 12,
+				VerticalOptions = LayoutOptions.End,
+				Children = 
+				{
+					facebookButton,
+					releaseVersion
+				}
+			};
+
+			return new StackLayout
+			{
+				Padding = new Thickness(20,40),
+				Children = 
+				{
+					firstLayout,
+					secondLayout
+				}
+			};
+		}
+	}
+}
+
