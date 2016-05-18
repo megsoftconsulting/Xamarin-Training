48656b2e3bad1c97a35c92d357d3eadce375284e
Luis Alberto Pena Nunez <luis@megsoftconsulting.com>
2016-05-18 09:58:04 -0500

    Enabled the Login screen for Android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
@@ -7,11 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
 
 namespace XForms.Droid
 {
 	[Activity (Label = "XForms.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	public class MainActivity : FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
