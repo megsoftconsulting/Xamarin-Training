cd92c7fcefa6f63ba3331a32b486586bb7cca98c
Luis Alberto Pena Nunez <luis@megsoftconsulting.com>
2016-05-24 17:02:21 -0500

    wip

---
+++ XForms/XFormsApp.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XForms
+{
+	public partial class XFormsApp : Application
+	{
+		public XFormsApp ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
