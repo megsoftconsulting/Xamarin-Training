1db62741f0fbc13ee8bc60fe6d6c5aab5e96aeb3
Luis Alberto Pena Nunez <luis@megsoftconsulting.com>
2016-05-25 23:00:23 -0500

    WIP: PaymentScreenListXAML

---
+++ XForms/Screens/PaymentList/XAML/PaymentScreenListXAML.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XForms
+{
+	public partial class PaymentScreenListXAML : ContentPage
+	{
+		public PaymentScreenListXAML ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
